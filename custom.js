dd0c54a304c4d12aca9ebb5a7429027cb9ed0040
jaspreet tpss <jaspreetstpss@gmail.com>
2025-08-05 22:58:31 +0530

    update basic layout

--- custom.js
+++ custom.js
@@ -12,3 +12,21 @@ function toggleHighlight() {
   const element = document.getElementById("slideNavigation");
   element.classList.toggle("slideMenu");
 }
+
+// ====== Hero section ======
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
+
+const video = document.getElementById("scrollVideo");
+const duration = 300; // number of video frames to scroll through
+
+ScrollTrigger.create({
+  trigger: "section",
+  start: "top top",
+  end: "bottom bottom",
+  scrub: true,
+  onUpdate: (self) => {
+    const scrollFraction = self.progress;
+    video.currentTime = scrollFraction * video.duration;
+  },
+});
